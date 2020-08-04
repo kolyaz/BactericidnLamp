d6dae27fa76f2cf31770f7e1a83f6ac286837a0c
Караван Николай Александрович <nkaravan@nevatom.ru>
2020-08-04 15:37:35 +0700

    9

--- src/lamp.cpp
+++ src/lamp.cpp
@@ -16,6 +16,7 @@ class Lamp
     {
        //LampNumber=LampResurs_St=LampResurs_Set=LampResusr_Res=LampResusr_OSt=0;
     }
+    int LampPinOut;                                    //Выход лампы
     int LampResurs_St;                                 //Ресурсное время лампы    
     int LampResurs_Set;                                //Ручное выставление ресурсного вермени лампы    
     int LampResusr_Res;                                //Сброс отработанного времени на выставленное лампы  
